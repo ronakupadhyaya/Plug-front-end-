4a1179ffc064099c53111c61cfbf49a19ccd4a31
Ronak Upadhyaya <rdupadhy@usc.edu>
2017-08-17 16:11:20 -0700

    m

--- js/AppNavigator.js
+++ js/AppNavigator.js
@@ -93,7 +93,7 @@ class AppNavigator extends Component {
         />
         <RouterWithRedux>
           <Scene key="root">
-            <Scene key="login" component={Login} hideNavBar />
+            <Scene key="login" component={Login} hideNavBar initial={true} />
             <Scene key="signUp" component={SignUp} />
             <Scene key="needhelp" component={NeedHelp} />
             <Scene key="home" component={Home} />
@@ -111,7 +111,7 @@ class AppNavigator extends Component {
             <Scene key="timeline" component={Timeline} />
             <Scene key="widgets" component={Widgets} />
             <Scene key="newProject" component={NewProject} />
-            <Scene key="searchCollab" component={SearchCollab}  initial={true}/>
+            <Scene key="searchCollab" component={SearchCollab} />
           </Scene>
         </RouterWithRedux>
       </Drawer>
